05b01682a83770b44b5def8adf114ae24859eb33
SkwalExe <support@skwal.net>
2022-09-18 07:23:29 +0200

    fix bug on windows version

--- src/terminal_size.cpp
+++ src/terminal_size.cpp
@@ -8,21 +8,22 @@
 #if defined(_WIN32)
 #define WIN32_LEAN_AND_MEAN
 #define VC_EXTRALEAN
-#include <Windows.h>
+#include <windows.h>
 #elif defined(__linux__)
 #include <sys/ioctl.h>
 #endif // Windows/Linux
 
-void get_terminal_size(int& width, int& height) {
+void get_terminal_size(int &width, int &height)
+{
 #if defined(_WIN32)
     CONSOLE_SCREEN_BUFFER_INFO csbi;
     GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-    width = (int)(csbi.srWindow.Right-csbi.srWindow.Left-1);
-    height = (int)(csbi.srWindow.Bottom-csbi.srWindow.Top);
+    width = (int)(csbi.srWindow.Right - csbi.srWindow.Left - 1);
+    height = (int)(csbi.srWindow.Bottom - csbi.srWindow.Top);
 #elif defined(__linux__)
     struct winsize w;
     ioctl(fileno(stdout), TIOCGWINSZ, &w);
     width = (int)(w.ws_col);
-    height = (int)(w.ws_row-2);
+    height = (int)(w.ws_row - 2);
 #endif // Windows/Linux
 }
